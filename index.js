d0c190f5537b8e07b1de23e3e1f21ec509cc3b21
Alexkawai <leha.iuganov@yandex.ru>
2021-12-22 13:42:26 +0300

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 // Import stylesheets
 import './style.css';
+import ReactDom from 'react-dom';
+import React from 'react';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById('hi');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+class ClickButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.press = this.press.bind(this);
+  }
+  press() {
+    console.log(this);
+    alert('Hello React!');
+  }
+  render() {
+    return <button onClick={this.press}>Click</button>;
+  }
+}
+ReactDom.render(<ClickButton />, document.getElementById('app'));
